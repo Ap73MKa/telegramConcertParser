385cee6c52848eb260ea0ae2612d4d0732b8f809
Ap73MKa <artyom.vlasov.05@mail.ru>
2022-10-06 10:40:33 +0300

    FIX: fix delete method

--- bot/database/methods/delete.py
+++ bot/database/methods/delete.py
@@ -5,6 +5,6 @@ from .get import get_concert_by_id
 def delete_concert_by_id(concert_id: int) -> None:
     session = Database().session
     concert = get_concert_by_id(concert_id)
-    if concert and concert.session:
-        session.delete(concert.session)
+    if concert:
+        session.delete(concert)
         session.commit()
