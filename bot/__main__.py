3d317244fb8842ef9169cb4d6102a6468822e95b
Ap73MKa <artyom.vlasov.05@mail.ru>
2023-01-28 12:41:01 +0300

    UPDATE: merge branches

--- bot/__main__.py
+++ bot/__main__.py
@@ -28,3 +28,4 @@ if __name__ == '__main__':
     log_path = PathManager.get('bot/logs/{time}.log')
     logger.add(log_path, format="{time} {level} {message}", rotation="10:00", compression="zip", retention="3 days")
     start_telegram_bot()
+
